d6c38c2ef35d219e5fa297b5899abd381d56e874
Sanket6220 <110032088+Sanket6220@users.noreply.github.com>
2022-07-29 15:09:45 +0530

    Add files via upload

---
+++ Day 5/Task 1/Controllers/EmployeeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication7.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication7.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        EmployeeDContext _context;
+
+        public EmployeeController(EmployeeDContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var empList = await _context.employees.ToListAsync();
+            return Ok(empList);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetEmployeeById(int id)
+        {
+            var empObj = await _context.employees.FindAsync(id);
+
+            if (empObj != null)
+                return Ok(empObj);
+            else
+                return NotFound("Employee Id does not exists.");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddEmployee(Employee obj)
+        {
+            await _context.employees.AddAsync(obj);
+            await _context.SaveChangesAsync();
+            return Ok("New Employee details are saved.");
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateStudent(Employee obj)
+        {
+            _context.employees.Update(obj);
+            await _context.SaveChangesAsync();
+            return Ok("Employee details updated.");
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteEmployee(int id)
+        {
+            var empObj = await _context.employees.FindAsync(id);
+            _context.employees.Remove(empObj);
+            await _context.SaveChangesAsync();
+            return Ok("Employee deleted.");
+        }
+    }
+}
---
+++ Day 5/Task 1/Models/EntityModelClass.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace WebApplication7.Models
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string Job { get; set; }
+        public int Salary { get; set; }
+        public int Deptno { get; set; }
+    }
+
+    public class EmployeeDContext : DbContext
+    {
+        public DbSet<Employee> employees { get; set; }
+
+        public EmployeeDContext(DbContextOptions<EmployeeDContext> options) : base(options)
+        {
+
+        }
+    }
+}
